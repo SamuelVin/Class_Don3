fae0cf0eb2b49480d6ff13fc205d62b51056a1ad
jpq122 <143190170+jpq122@users.noreply.github.com>
2023-09-12 16:40:54 -0400

    Add files via upload

---
+++ events.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mar 12 Septembre 2023 à 20:40
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `smiley face`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `events`
+--
+
+CREATE TABLE `events` (
+  `Id` int(11) NOT NULL,
+  `Date` varchar(10) NOT NULL,
+  `Lieu` varchar(255) NOT NULL,
+  `NomEvenement` varchar(255) NOT NULL,
+  `Departement` varchar(255) NOT NULL,
+  `SatisfactionVert` int(11) NOT NULL,
+  `SatisfactionJaune` int(11) NOT NULL,
+  `SatisfactionRouge` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16le;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `events`
+--
+ALTER TABLE `events`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
