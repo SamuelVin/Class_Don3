a414d76a6f9b687d8beb026dd1a4a5208497cb88
SamuelVin <samuel.vinette.01@edu.cegeptr.qc.ca>
2023-09-15 10:09:30 -0400

    Statisfaction_Student -> Written, Php Form Needed!

--- Web/Satisfaction_Student.php
+++ Web/Satisfaction_Student.php
@@ -7,8 +7,52 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
+<body class="container-fluid">
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <input type="password" style="text-align:center; margin-top: 1%;"></input> 
+    <input type="submit" style="text-align:center;" value=" Retour "></input>
+    <a href="../index.php">index.php</a>
+</form>
+
+<h1 style="text-align:center; margin-top: 8%; margin-bottom: 10%">Comment était votre expérience?</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4 justify-content center">
+            <img src="../Image/happy.bmp" class="d-flex" style="margin: auto;"/>
+            <input type="radio" name="choose" class="d-flex" style="margin: auto;"></input>
+        </div>
+        <div class="col-sm-4">
+            <img src="../Image/mid.bmp" class="d-flex" style="margin: auto;"/>
+            <input type="radio" name="choose" class="d-flex" style="margin: auto;"></input>
+        </div>
+        <div class="col-sm-4">
+            <img src="../Image/angry.bmp" class="d-flex" style="margin: auto;"/>
+            <input type="radio" name="choose" class="d-flex" style="margin: auto;"></input>
+        </div>
+    </div>
+</div>
+
     
+
+<!--
+    <div class="d-flex align-items-center justify-content-center">
+        <div class="justify-content-center">
+            <img src="../Image/happy.bmp"/><br>
+            <input type="radio" name="choose" style="padding-left: 60px"></input>
+        </div>
+        <div>
+            <img src="../Image/mid.bmp"/><br>
+            <input type="radio" name="choose"></input>
+        </div>
+        <div>
+            <img src="../Image/angry.bmp"/><br>
+            <input type="radio" name="choose"></input>
+        </div>
+    </div>
+-->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
--- index.php
+++ index.php
@@ -8,8 +8,13 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Titre</h1>
-    <h2>Test</h2>
+    <h1>Index.php</h1>
+    <h4>This is the main page/ connexion</h2>
+
+    <a href="Web/Admin.php">Admin.php</a><br>
+    <a href="Web/Satisfaction_Employee.php">Satisfaction_Employee.php</a><br>
+    <a href="Web/Satisfaction_Student.php">Satisfaction_Student.php</a>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
